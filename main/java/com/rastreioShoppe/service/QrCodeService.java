1da5430d6c81b144e41305bd912ac9dff5129b97
Pedro <104221500+pmd1308@users.noreply.github.com>
2024-10-07 07:36:30 -0300

    Update QrCodeService.java

--- main/java/com/rastreioShoppe/service/QrCodeService.java
+++ main/java/com/rastreioShoppe/service/QrCodeService.java
@@ -7,7 +7,5 @@ Penso na seguinte logica
 
 # Aqui seria implementado a logica para os getters serem chamados nos parametros
 
-# O objeto é destruido ao passar no ultimo coletor, e dado que apesar de ser fodastico, não tá rolando grana, e sou que nem puta, quer gemido paga mais jjjjkkkkkkk
-
-É muito bom ser um gênio, ainda mais quando tô só pelo merito kkkkkkkkkkk
+# O objeto é destruido ao passar no ultimo coletor
 */
